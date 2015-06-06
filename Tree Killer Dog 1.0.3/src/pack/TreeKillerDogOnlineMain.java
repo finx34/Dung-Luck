4ab29c4914720c2abd213212dc3e9cea7c885f9e
Gabriel <Gabriel@idea-PC>
2015-06-06 01:49:16 -0300

    first penis

--- Tree Killer Dog 1.0.3/src/pack/TreeKillerDogOnlineMain.java
+++ Tree Killer Dog 1.0.3/src/pack/TreeKillerDogOnlineMain.java
@@ -5,7 +5,7 @@ import ventanas.Ventana;
 
 public class TreeKillerDogOnlineMain {
 	  public static void main(String args[]) throws UnknownHostException {
-		  
+		  System.out.println("Peeeneeees");
 		  Ventana.launch("Tree Killer Dog","Direccion IP","Puerto");
 		  Ventana.setAccion1(new AccionConectar(), "Conectar");
 		  Ventana.setAccion2(new AccionSalir(), "Salir");
